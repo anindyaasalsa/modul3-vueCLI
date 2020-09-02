98d401d8c8a244b607608fcd21ae1ca32e04ce3a
anindyaasalsa <anindya_salsabilla_27rpl@student.smktelkom-mlg.sch.id>
2020-09-03 00:18:58 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <First msg="First Component"></First>
+    <First msg="Anindya XII RPL 2 / 06"></First>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import First from './components/First.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    First
   }
 }
 </script>
---
+++ src/components/First.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<style scoped>
+    h1 {
+        color: white;
+        padding: 10px 50px;
+        background-color: #6faf00;
+    }
+</style>
+
+<script>
+export default {
+    name: "First",
+    props: {
+        msg: String
+    }
+}
+</script>
